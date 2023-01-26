e6d55b99f102afca508faf10e5a94d643fb8af7b
khadija-edris <118714830+khadija-edris@users.noreply.github.com>
2023-01-26 11:46:09 +0200

    add lable name's Employee Management

--- khadiga edris task database/Employee.Designer.cs
+++ khadiga edris task database/Employee.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,12 +54,23 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Employee Management System Version 10 ";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Teal;
+            this.label2.Location = new System.Drawing.Point(663, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(219, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Employee Management";
+            // 
             // Employee
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1386, 936);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -68,6 +80,7 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,5 +88,6 @@
 
         private Panel panel1;
         private Label label1;
+        private Label label2;
     }
 }
