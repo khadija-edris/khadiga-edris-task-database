dc13a68e856296aa88ffcc10fecadec219a94b2f
khadija-edris <118714830+khadija-edris@users.noreply.github.com>
2023-01-27 15:27:22 +0200

    effect to textbox

--- khadiga edris task database/Form1.Designer.cs
+++ khadiga edris task database/Form1.Designer.cs
@@ -147,7 +147,7 @@
         private Panel panel1; 
         private Label label1; 
         private PictureBox pictureBox1; 
-        private TextBox textBox1;
+        private TextBox textBox1; 
         private TextBox textBox2;
         private Label label2;
         private Label label3;
