ce6a100b0d779d8f52b7abbc8a4f33190c8d15fd
khadija-edris <118714830+khadija-edris@users.noreply.github.com>
2023-01-27 15:44:31 +0200

    show picturebox

--- khadiga edris task database/Departements.Designer.cs
+++ khadiga edris task database/Departements.Designer.cs
@@ -85,13 +85,13 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(159, 154);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(86, 60);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 37;
-            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image"))); 
+            this.pictureBox1.Location = new System.Drawing.Point(159, 154); 
+            this.pictureBox1.Name = "pictureBox1"; 
+            this.pictureBox1.Size = new System.Drawing.Size(86, 60); 
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom; 
+            this.pictureBox1.TabIndex = 37; 
+            this.pictureBox1.TabStop = false; 
             // 
             // panel2
             // 
