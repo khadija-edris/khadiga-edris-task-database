ac1c162c6e154cacfef41d41637652b2a6f54c75
khadija-edris <118714830+khadija-edris@users.noreply.github.com>
2023-01-27 15:17:07 +0200

    private System.ComponentModel.IContainer components is null

--- khadiga edris task database/Form1.Designer.cs
+++ khadiga edris task database/Form1.Designer.cs
@@ -5,12 +5,12 @@
         /// <summary>
         ///  Required designer variable.
         /// </summary>
-        private System.ComponentModel.IContainer components = null; 
+        private System.ComponentModel.IContainer components = null;   
 
         /// <summary>
         ///  Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param> 
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
