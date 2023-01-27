9656176263da7f1217890d290845a0446b687f6c
khadija-edris <118714830+khadija-edris@users.noreply.github.com>
2023-01-27 15:24:34 +0200

    means the panel in form1.designer.cs to show the design of form1 with good method

--- khadiga edris task database/Form1.Designer.cs
+++ khadiga edris task database/Form1.Designer.cs
@@ -144,7 +144,7 @@
 
         #endregion
 
-        private Panel panel1;
+        private Panel panel1; 
         private Label label1;
         private PictureBox pictureBox1;
         private TextBox textBox1;
